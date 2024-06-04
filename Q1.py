785666c8dca6f0de173ffa5769742ae2f41a2d2c
Aswath S <139672350+AswathSundaram@users.noreply.github.com>
2024-06-04 12:01:18 +0530

    Q1.py

--- Q1.py
+++ Q1.py
@@ -2,10 +2,10 @@
 def minimumBribes(q):
     bribe = 0
     for i in range(len(q)):
-        if q[i] - (i + 1) > 2:
-            print("Too chaotic")
+        if q[i] - (i + 1) > 2: 
+            print("Too chaotic") #if the movement is >2 it is chaotic
             return
-        for j in range(max(0, q[i] - 2), i):
+        for j in range(max(0, q[i] - 2), i): #it checks if the previous 2 values(if not exist then possible previous values either 0 or 1)is > than present
             if q[j] > q[i]:
                 bribe += 1
     print(bribe)
