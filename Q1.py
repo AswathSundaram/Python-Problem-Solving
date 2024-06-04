bd945d710b60b836aeda3b4aebb2b547f5b3a40c
Aswath S <139672350+AswathSundaram@users.noreply.github.com>
2024-06-04 10:38:33 +0530

    Q1.py

--- Q1.py
+++ Q1.py
@@ -1,1 +1,23 @@
 
+def minimumBribes(q):
+    bribe = 0
+    for i in range(len(q)):
+        if q[i] - (i + 1) > 2:
+            print("Too chaotic")
+            return
+        for j in range(max(0, q[i] - 2), i):
+            if q[j] > q[i]:
+                bribe += 1
+    print(bribe)
+
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
+
